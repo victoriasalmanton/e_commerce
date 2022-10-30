86f8c7fb2878987e940c8a2c0d354f12786285cc
victoriasalmanton <109481442+victoriasalmanton@users.noreply.github.com>
2022-10-30 15:54:49 -0300

    Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,8 @@
 let arrayOfProducts = [];
+const shippingType1 = document.getElementById("shippingType1");
+const shippingType2 = document.getElementById("shippingType2");
+const shippingType3 = document.getElementById("shippingType3");
 
-/* 5.1 toma cada objeto del array que son productos y le añade una tabla que muestra
- imagen, nombre, cantidad, costo.
-
-  5.3 Onchange en input de cantidad ejecuta la funcion cuando el valor del input se cambia.
-  Ejecuta onCountChange que tiene evento y id como parametro */
 const showCartItems = () => {
   const htmlCartItemsToAppend = arrayOfProducts
     .map(
@@ -15,7 +13,7 @@ const showCartItems = () => {
             <td>${product.currency} ${product.unitCost}</td>
             <td><input type="number" value="${
               product.count
-            }" min="1" class="form-control" style="max-width:80px" onchange="onCountChange(event,${
+            }" min="1" class="form-control" style="max-width:80px" id="countBox" onchange="onCountChange(event,${
           product.id
         })">
             </td>
@@ -28,15 +26,58 @@ const showCartItems = () => {
 
   document.getElementById("table").innerHTML = htmlCartItemsToAppend;
 };
-/* 5.3 e es el evento que se ejecuta 
-    target es el elemento html que lanzo el evento como getElementById
 
-  *newCount es el input cambiado
-   
-   OnCountChange busca en array de productos el producto con igual id al del parametro
-   y hace que el count de ese producto tome la nueva cantidad que se inserto en el input
-   llama de nuevo a showCartItems porque el calculo del Subtotal es el producto de 
-   la cantidad por el precio unitario y ahi los modifica*/
+//6.3
+function showSuccessAlert() {
+  const alert = document.getElementById("successAlert");
+
+  alert.classList.remove("d-none");
+
+  setTimeout(() => {
+    alert.classList.add("d-none");
+  }, 3000);
+}
+
+/* 6.1 visualizar subtotal general costo de envio y total a pagar
+
+*inicializa subtotal en 0. 
+* Con forEach toma cada producto y agrega a subtotal el producto del precio por cantidad
+*inicializa una variable de porcentajes en 0 y le pone el valor condicionado por el checked
+* el total a pagar es el subtotal por el costo de envio
+* con innher html los mostramos en pantalla
+
+* llamamos a esta misma funcion cuando cambia la cantidad del producto seleccionado,
+ cuando se cambia el tipo de envio y cuando carga la pagina*/
+
+const updateSummary = () => {
+  let subtotal = 0;
+
+  // calcular subtotal recorriendo array
+  arrayOfProducts.forEach((product) => {
+    subtotal += product.unitCost * product.count;
+  });
+
+  let shippingPercentage = 0;
+  //envio premium
+  if (shippingType1.checked) {
+    shippingPercentage = 0.15;
+    //express
+  } else if (shippingType2.checked) {
+    shippingPercentage = 0.07;
+    //Standard
+  } else if (shippingType3.checked) {
+    shippingPercentage = 0.05;
+  }
+
+  // mostrar subtotal
+  document.getElementById("subtotal").innerHTML = `USD ${subtotal}`;
+  document.getElementById("costoEnvio").innerHTML = `USD ${Math.round(
+    subtotal * shippingPercentage
+  )}`;
+  document.getElementById("total").innerHTML = `USD ${
+    subtotal + Math.round(subtotal * shippingPercentage)
+  } `;
+};
 
 const onCountChange = (e, productID) => {
   const newCount = e.target.value;
@@ -47,15 +88,142 @@ const onCountChange = (e, productID) => {
   productToModify.count = newCount;
 
   showCartItems();
+  updateSummary();
 };
 
-/* 5.1 atributo defer hace que el script se descargue en paralelo al resto pero 
-se ejectua luego de que la pag se analice. Ahorra el add event listener DOMContentLoaded
-
-Hacemos solicitud al json, guarda en array que es vacio y llama a showCartItem*/
 getJSONData(CART_INFO_URL + "25801" + EXT_TYPE).then(function (resultObj) {
   if (resultObj.status === "ok") {
     arrayOfProducts = resultObj.data.articles;
     showCartItems();
+    updateSummary();
+  }
+});
+
+//6.1 //
+
+shippingType1.addEventListener("click", updateSummary);
+shippingType2.addEventListener("click", updateSummary);
+shippingType3.addEventListener("click", updateSummary);
+
+//*6.2 modal con forma de pago y deshabilitar campos innecesarios.
+
+//obtenemos id de los botones de forma de pago y los campos del modal  */
+
+// radio buttons
+const buttonCredit = document.getElementById("buttonCredit");
+const transfer = document.getElementById("transfer");
+
+// fields credit
+const cardNumber = document.getElementById("cardNumber");
+const securityCode = document.getElementById("securityCode");
+const expiration = document.getElementById("expiration");
+
+// fields transfer
+const accountNumber = document.getElementById("accountNumber");
+
+/* 6.2 cuando se hace click en tarjeta de credito se habilitan los input correspondientes
+ y se desabilidan los de la otra forma de pago.
+ se muestra en el button fuera del modal el metodo seleccionado  */
+
+buttonCredit.addEventListener("click", () => {
+  cardNumber.disabled = false;
+  securityCode.disabled = false;
+  expiration.disabled = false;
+
+  accountNumber.disabled = true;
+
+  document.getElementById("formaDePago").innerHTML = "Tarjeta de Crédito";
+});
+
+transfer.addEventListener("click", () => {
+  cardNumber.disabled = true;
+  securityCode.disabled = true;
+  expiration.disabled = true;
+
+  accountNumber.disabled = false;
+
+  document.getElementById("formaDePago").innerHTML = "Transferencia bancaria";
+});
+
+/// 6.3 //
+const street = document.getElementById("street");
+const streetNumber = document.getElementById("streetNumber");
+const intersection = document.getElementById("intersection");
+const paymentMethod = document.getElementById("paymentMethod");
+
+// si no hay value le agrega clase de invalid y si tiene le agrega valid y le saca la clase invalid
+const inputHasValue = (input) => {
+  if (!input.value) {
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    return false;
+  }
+
+  input.classList.add("is-valid");
+  input.classList.remove("is-invalid");
+  return true;
+};
+
+const validateForm = () => {
+  let isValid = true;
+
+  if (!inputHasValue(street)) isValid = false;
+  if (!inputHasValue(streetNumber)) isValid = false;
+  if (!inputHasValue(intersection)) isValid = false;
+
+  if (!buttonCredit.checked && !transfer.checked) {
+    buttonCredit.classList.add("is-invalid");
+    transfer.classList.add("is-invalid");
+    paymentMethod.classList.add("is-invalid");
+    isValid = false;
+  } else {
+    buttonCredit.classList.remove("is-invalid");
+    transfer.classList.remove("is-invalid");
+    paymentMethod.classList.remove("is-invalid");
+  }
+
+  if (buttonCredit.checked) {
+    if (!inputHasValue(cardNumber)) isValid = false;
+    if (!inputHasValue(securityCode)) isValid = false;
+    if (!inputHasValue(expiration)) isValid = false;
+  } else {
+    cardNumber.classList.remove("is-invalid", "is-valid");
+    securityCode.classList.remove("is-invalid", "is-valid");
+    expiration.classList.remove("is-invalid", "is-valid");
+  }
+
+  if (transfer.checked) {
+    if (!inputHasValue(accountNumber)) isValid = false;
+  } else {
+    accountNumber.classList.remove("is-invalid", "is-valid");
+  }
+
+  return isValid;
+};
+
+/* Cuando apreto el boton de finalizar compra se aejecuta una funcion que aplica 
+un for each a todos los input y a los radios que logre que cada vez que se hace click o se cambia algun valor, 
+se llame a la funcion de validacion*/
+document.getElementById("checkout").addEventListener("click", () => {
+  [
+    street,
+    streetNumber,
+    intersection,
+    paymentMethod,
+    cardNumber,
+    securityCode,
+    expiration,
+    accountNumber,
+  ].forEach((input) => input.addEventListener("keyup", validateForm));
+
+  // En el caso de los buttons es un click
+  [buttonCredit, transfer].forEach((input) =>
+    input.addEventListener("click", validateForm)
+  );
+
+  //sucess alert
+  const isValid = validateForm();
+  if (isValid) {
+    showSuccessAlert();
   }
 });
